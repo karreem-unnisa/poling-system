2ba2a23a920fbfa5d996031de1db38a5656e3984
Kareemunnisa <mailkareemunnisa@gmail.com>
2025-09-29 15:21:23 +0530

    Package.json update

--- poll/frontend/src/socket.js
+++ poll/frontend/src/socket.js
@@ -1,5 +1,5 @@
 
 import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const BACKEND_URL = import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
 export const socket = io(BACKEND_URL);
